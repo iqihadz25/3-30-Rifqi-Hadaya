20ff9dad18bc71e687754a833ce54b94d608da97
iqihadz25 <91859177+iqihadz25@users.noreply.github.com>
2022-01-25 07:14:23 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Rifqi Hadaya | About</title>
+</head>
+<body>
+    <h1> Halaman About </h1>
+    <h3> <?php echo $nama; ?> </h3>
+    <p> <?php echo $email; ?> </p>
+    <img src="images/<?php echo $gambar; ?>" alt="<?php echo $nama; ?>" width="200px">
+
+    <script src="js/script.js"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "nama" => "Mas Iqi Ganteng",
+        "email" => "rifqi@smktelkom-pwt.sch.id",
+        "gambar" => "FOto Jaran.jpeg"
+    ]);
+});
+
+Route::get('/gallery', function () {
+    return view('gallery');
+});
+
